ad44bc76a638428c7874d21e854a13259b6342c9
Sanoop Jose <Sanoop Jose>
2017-04-24 11:25:25 +0530

    Component added

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class ReactHelloWorld extends React.Component {
+  render() {
+    return (
+      <div> Hello world!! </div>
+    )
+  }
+}
+export default ReactHelloWorld;
